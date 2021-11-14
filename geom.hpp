7eeff0a481f08d36a43b30507f7d26d64e1e2a05
saashkoo <85512383+saashkoo@users.noreply.github.com>
2021-11-14 14:14:55 +0200

    Update geom.hpp

--- geom.hpp
+++ geom.hpp
@@ -34,10 +34,9 @@ struct Line {
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
     Line(const Point& p1, const Point& p2) {
-        B = (p2.y - p1.y)/(p2.x - p1.x); 
-        C = p1.y - p1.x*((p2.y - p1.y)/(p2.x - p1.x));
-        A = -(A+C); 
-        
+        A = (p2.y - p1.y)/(p2.x - p1.y); 
+        B = -1; 
+        C = p1.y - p1.x*((p2.y - p1.y)/(p2.x - p1.y));
     }
 
     bool parallel(const Line& other) const {
