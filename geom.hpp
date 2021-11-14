15239ce45b4193999a20df8589619f8ccaa376d3
saashkoo <85512383+saashkoo@users.noreply.github.com>
2021-11-14 14:33:00 +0200

    Update geom.hpp

--- geom.hpp
+++ geom.hpp
@@ -41,9 +41,7 @@ struct Line {
 
     bool parallel(const Line& other) const {
         if(other.A == 0 || other.B == 0){
-            if(A != 0 && B != 0){
-            return A*other.B == B*other.A;}
-            else{return false;}
+            return (A*other.B) == (B*other.A);
         }
         return (A/other.A) == (B/other.B);
     }
