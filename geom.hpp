facd5c6851d5508405f3f3a3f242f4a313b09e15
saashkoo <85512383+saashkoo@users.noreply.github.com>
2021-11-14 13:55:37 +0200

    Update geom.hpp

--- geom.hpp
+++ geom.hpp
@@ -34,9 +34,9 @@ struct Line {
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
     Line(const Point& p1, const Point& p2) {
-        C = (p2.y - p1.y)/(p2.x - p1.x); 
-        A = p1.y - p1.x*((p2.y - p1.y)/(p2.x - p1.x));
-        B = -(A+C); 
+        B = (p2.y - p1.y)/(p2.x - p1.x); 
+        C = p1.y - p1.x*((p2.y - p1.y)/(p2.x - p1.x));
+        A = -(A+C); 
         
     }
 
